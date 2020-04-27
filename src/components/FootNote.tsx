226a01a6f017782162f753b56c48cea227c6408f
Lukas Bach <lbach@outlook.de>
2020-04-27 15:14:11 +0200

    Display version

--- src/components/FootNote.tsx
+++ src/components/FootNote.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
+import pkg from '../../package.json';
 
 export const FootNote: React.FC<{}> = props => {
   return (
     <footer>
+      Vileo {pkg.version} -{' '}
       Made by Lukas Bach.{' '}
       {/* eslint-disable-next-line react/jsx-no-target-blank */}
       <a href="https://lukasbach.com" target="_blank">lukasbach.com</a>{' '}
