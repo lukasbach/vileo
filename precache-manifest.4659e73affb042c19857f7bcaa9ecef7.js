bcf87c4f41bc0e0864be714e64becb2355156a6f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-28 09:06:20 +0000

    Deploy lukasbach/vileo to github.com/lukasbach/vileo.git:gh-pages

---
+++ precache-manifest.4659e73affb042c19857f7bcaa9ecef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "228f044645cc63306b138d106b7a0526",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4a361939c26f16861195",
+    "url": "./static/css/main.7b71c17a.chunk.css"
+  },
+  {
+    "revision": "8b1c027382864236e787",
+    "url": "./static/js/2.54952342.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.54952342.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a361939c26f16861195",
+    "url": "./static/js/main.af841d71.chunk.js"
+  },
+  {
+    "revision": "8644dd1ed3efc0ea85d6",
+    "url": "./static/js/runtime-main.87e1c4ec.js"
+  }
+]);
